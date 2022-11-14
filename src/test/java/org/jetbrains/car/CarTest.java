02548c94f520c8e49a1b53dacf3e7fc49930fec7
Ilya Brovarnik <ilya.brovarnik@gmail.com>
2022-11-14 21:07:25 +0100

    add CarTest

--- src/test/java/org/jetbrains/car/CarTest.java
+++ src/test/java/org/jetbrains/car/CarTest.java
@@ -1,2 +1,52 @@
-package org.jetbrains.car;public class CarTest {
+package org.jetbrains.car;
+
+import org.jetbrains.utils.Constants;
+import org.jetbrains.utils.TestConstants;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+import static org.jetbrains.utils.TestConstants.EPS;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CarTest {
+    static Random random;
+
+    @BeforeAll
+    static void initTests() {
+        random = new Random(TestConstants.TESTS_RANDOM_SEED);
+    }
+
+    @Test
+    void testCarCreation() {
+        Car electricCar = new ElectricCar(Constants.MIN_LOCATION, 1);
+        Car petrolCar = new PetrolCar(Constants.MIN_LOCATION, 1);
+        assertEquals(electricCar.energyThreshold, Constants.ELECTRIC_CAR_ENERGY_THRESHOLD);
+        assertEquals(petrolCar.energyThreshold, Constants.PETROL_CAR_ENERGY_THRESHOLD);
+        assertEquals(electricCar.getEnergyValue(), Constants.MAX_ENERGY);
+        assertEquals(petrolCar.getEnergyValue(), Constants.MAX_ENERGY);
+    }
+
+    @Test
+    void testCarCreationIllegal() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ElectricCar(Constants.MIN_LOCATION, 0);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PetrolCar(Constants.MIN_LOCATION, 0);
+        });
+    }
+
+    @Test
+    void testCarDriveRefuel() {
+        Car car = new ElectricCar(Constants.MIN_LOCATION, 2.0);
+        double destination = Constants.MIN_LOCATION + Constants.MAX_ENERGY / 2.0;
+        assert(car.needsEnergy(destination));
+        car.driveTo(destination);
+        assert(car.getLocation() == destination);
+        assert(car.getEnergyValue() < EPS);
+        car.refuel();
+        assert(car.getEnergyValue() == Constants.MAX_ENERGY);
+    }
 }
--- src/test/java/org/jetbrains/utils/TestConstants.java
+++ src/test/java/org/jetbrains/utils/TestConstants.java
@@ -1,2 +1,6 @@
-package org.jetbrains.utils;public class Constants {
+package org.jetbrains.utils;
+
+public abstract class TestConstants {
+    static public final int TESTS_RANDOM_SEED = 0;
+    static public final double EPS = 1e-7;
 }
